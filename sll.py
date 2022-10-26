08558bba7e7c99bcc895130799030c96bec3cf25
Jessica Allman-LaPorte <90071482+JessicaAllman-LaPorte@users.noreply.github.com>
2022-10-26 18:31:06 -0400

    wrote insert_at_index()

--- sll.py
+++ sll.py
@@ -99,7 +99,12 @@ class LinkedList:
         If the linked list contains N nodes (the sentinel node is not included
         in this count), valid indices for this method are [0, N] inclusive.
         """
-        pass
+        curr = self._head
+        for num in range(index):
+            curr = curr.next
+        new_node = SLNode(value)
+        new_node.next = curr.next
+        curr.next = new_node
 
     def remove_at_index(self, index: int) -> None:
         """
@@ -150,31 +155,31 @@ class LinkedList:
 
 if __name__ == "__main__":
 
-    print("\n# insert_front example 1")
-    test_case = ["A", "B", "C"]
-    lst = LinkedList()
-    for case in test_case:
-        lst.insert_front(case)
-        print(lst)
-
-    print("\n# insert_back example 1")
-    test_case = ["C", "B", "A"]
-    lst = LinkedList()
-    for case in test_case:
-        lst.insert_back(case)
-        print(lst)
-
-    # print("\n# insert_at_index example 1")
+    # print("\n# insert_front example 1")
+    # test_case = ["A", "B", "C"]
     # lst = LinkedList()
-    # test_cases = [(0, "A"), (0, "B"), (1, "C"), (3, "D"), (-1, "E"), (5, "F")]
-    # for index, value in test_cases:
-    #     print("Inserted", value, "at index", index, ": ", end="")
-    #     try:
-    #         lst.insert_at_index(index, value)
-    #         print(lst)
-    #     except Exception as e:
-    #         print(type(e))
+    # for case in test_case:
+    #     lst.insert_front(case)
+    #     print(lst)
     #
+    # print("\n# insert_back example 1")
+    # test_case = ["C", "B", "A"]
+    # lst = LinkedList()
+    # for case in test_case:
+    #     lst.insert_back(case)
+    #     print(lst)
+
+    print("\n# insert_at_index example 1")
+    lst = LinkedList()
+    test_cases = [(0, "A"), (0, "B"), (1, "C"), (3, "D"), (-1, "E"), (5, "F")]
+    for index, value in test_cases:
+        print("Inserted", value, "at index", index, ": ", end="")
+        try:
+            lst.insert_at_index(index, value)
+            print(lst)
+        except Exception as e:
+            print(type(e))
+
     # print("\n# remove_at_index example 1")
     # lst = LinkedList([1, 2, 3, 4, 5, 6])
     # print(f"Initial LinkedList : {lst}")
